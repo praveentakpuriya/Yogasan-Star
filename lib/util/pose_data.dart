e32e23bc150b794e2712f7b0e365156fd8363943
lalit <lalitpatidar211999@gmail.com>
2021-06-23 08:21:21 +0530

    begin 4 inter 3

--- lib/util/pose_data.dart
+++ lib/util/pose_data.dart
@@ -8,10 +8,9 @@ List<String> beginnerAsanas = [
 ];
 
 List<String> intermediateAsanas = [
-  'coming_soon'
-  // 'Dhanurasana',
-  // 'Padhastasana',
-  // 'Ustrasana',
+  'Dhanurasana',
+  'Padhastasana',
+  'Ustrasana',
   // 'Halasana',
   // 'Sarvangasana'
 ];
@@ -24,19 +23,33 @@ List<String> advanceAsanas = [
 ];
 
 var asanasInfo = {
-  'Trikonasana': 'info1',
-  'Vrikshasana': 'info2',
-  'Virbhadrasana': 'info3',
-  'Bhujangasana': 'info4',
+  'Trikonasana':
+      "Trikonasana is also called as triangle pose. The name trikonasana comes from the Sanskrit words trikona which means triangle and asana means posture. Different schools of yoga have different-different views about what trikonasana is and how it should be performed. It is a standing position providing deep stretch to the whole body parts",
+  'Vrikshasana':
+      "The name ‘Vrikshasana’ is comes from Sanskrit, in which ‘Vriksha’ means Tree and meaning of Asana is to seat, pose or posture. When this pose is performed effectively, it would seem that a tree. As it were, you stop like a tree in the last position of this stance. The leg that you are remaining on resembles a trunk attached to the ground with arms and the other leg and the head go about as branches and clears out. So it is called Tree pose. This Pose has a place with the Inverted Balancing gathering of stances. It is a standout amongst the most troublesome, intriguing and powerful yoga postures",
+  'Virbhadrasana':
+      "Virabhadra is a legendary character made by Lord Shiva, and this posture gets its name from that point. Virabhadrasana has profound established history and mythology connected to it. Meaning of “Vira” is hero, warrior, vigorous, courageous; 'Bhadra' means –Friend, good, auspicious and ‘Asana’ means seat, pose or posture. This pose is divided into three parts type 1, type 2 and type 3. Pronunciation of Virabhadrasana is veer-ah-bah-DRAHS-anna",
+  'Bhujangasana':
+      "Bhujangasana widely known as Cobra Pose is a backward bending pose. The Sanskrit name Bhujangasana is derived from two words. ‘Bhujanga’ stands for serpent while ‘asana’ means posture. The final position of Bhujangasana resembles a cobra with its hood raised and hence the name. It is an excellent pose for strengthening the spine along with many other benefits such as alleviating constipation, stimulating appetite, and massaging abdominal organs.",
   // 'Sukhasana': 'info5',
   // 'Tadasana': 'info6',
+  'Dhanurasana':
+      "The name Dhanurasana comes from the Sanskrit words Dhanura which means bow and Asana means posture or seat This body posture of this is named after the shape it takes-that of a bow Dhanurasana is also sometimes linked with upward wheel pose or Urdhva Chakrasana. The Bow or Dhanurasana raises both halves of the body at once, combining the movements of the Cobra and Locust, and countering the Plough and the Forward Bend, same as like an archer stringing a bow. In this you use your hands and arms to pull your trunk and legs up together to form a curve. It tones back muscles and maintains the elasticity of spine, improving posture and increasing vitality",
+  'Padhastasana':
+      "Pada hastasana is a stretching pose. It gives a lot of stretch to your thigh and calf muscle. It also gives a stretch to your lower back. Pada means foot and hast means hand, asana means to be in posture where you can stay firmly and comfortably for longer. Hence Padahastasana is the posture where the feet are touched by the hands.Padahastasana is also used in the practice of Suryanamaskara. Suryanamaskara is also known as sun salutation. It comprises of 12 steps. Padahastasana is the second and eleventh step among twelve steps of Suryanamaskara",
+  'Ustrasana':
+      "Ustrasana also known as Camel Pose is a deep backward bending pose. The name, ‘Ustrasana’ comes from the Sanskrit words, ustra and asana, where ustra stands for camel and asana stands for ‘posture. This pose is very beneficial for developing flexibility in the body, there are many other benefits of Ustrasana but first, let us know the steps to practice ‘Ustrasana’",
 };
 
 var asanasUrl = {
   'Trikonasana': 'https://youtu.be/5iBDuCYWIHg',
   'Vrikshasana': 'https://youtu.be/gFLlSIdlrtc',
   'Virbhadrasana': 'https://youtu.be/YbSoez-FLQU',
-  'Bhujangasana': 'info4',
+  'Bhujangasana': 'https://youtu.be/75eZwVHIL-U',
   // 'Sukhasana': 'info5',
   // 'Tadasana': 'info6',
+  //intermediate
+  'Dhanurasana': 'https://youtu.be/wgH2PdPnZGM',
+  'Padhastasana': 'https://youtu.be/Kyk0NEiCAIM',
+  'Ustrasana': 'https://youtu.be/ItWYVxm3Mss',
 };
